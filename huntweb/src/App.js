389c2b3c4cca4bad8a5e9a711353aa5d31ccb0fd
lzferreira <luiz.analista@hotmail.com>
2019-12-20 16:14:34 -0300

    listando

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './components/Header';
+import Main from  './pages/main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
